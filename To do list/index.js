04bfc99a96aab027f246e45dc82bca12e022161c
Lucas <undertalefan812@gmail.com>
2022-02-16 13:32:09 -0500

    m

--- To do list/index.js
+++ To do list/index.js
@@ -2,12 +2,12 @@ var btn = document.querySelector('#btn');
 var inputEl = document.querySelector("#input-el");
 var lisp = document.querySelector('#list');
 btn.addEventListener('click', function () {
+    var _a;
     var input = inputEl.value;
     inputEl.value = "";
     console.log(input);
     lisp.innerHTML += "<li>".concat(input, " <button id=\"btn2\">Done</button></li>");
-});
-var btn2 = document.querySelector('#btn2');
-btn2.addEventListener('click', function () {
-    console.log("Hello");
+    (_a = document.querySelector('#btn2')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
+        console.log("Hello");
+    });
 });
--- To do list/index.ts
+++ To do list/index.ts
@@ -2,17 +2,15 @@ const btn = document.querySelector('#btn')
 let inputEl = (document.querySelector("#input-el") as HTMLInputElement)
 let lisp = document.querySelector('#list')
 
-btn!.addEventListener('click', () => {
+btn!.addEventListener('click', () =>{
     const input = inputEl.value
     inputEl.value = ""
     console.log(input)
 
     lisp!.innerHTML += `<li>${input} <button id="btn2">Done</button></li>`
-    
+    document.querySelector('#btn2')?.addEventListener('click', () => {
+        console.log("Hello")
+    })
 })
 
-const btn2 = document.querySelector('#btn2')
 
-btn2!.addEventListener('click', () => {
-    console.log("Hello")
-})
